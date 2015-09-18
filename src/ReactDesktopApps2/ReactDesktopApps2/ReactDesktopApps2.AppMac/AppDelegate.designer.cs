7c5b2a1747207aaede5a14a06af8d2f31053ed3f
Darren Reid <darren.j.reid@gmail.com>
2015-09-18 21:46:12 +1000

    new proj + bundle

---
+++ src/ReactDesktopApps2/ReactDesktopApps2/ReactDesktopApps2.AppMac/AppDelegate.designer.cs
@@ -0,0 +1,10 @@
+
+namespace ReactDesktopApps2.AppMac
+{
+	// Should subclass MonoMac.AppKit.NSResponder
+	[MonoMac.Foundation.Register ("AppDelegate")]
+	public partial class AppDelegate
+	{
+	}
+}
+
---
+++ src/ReactDesktopApps2/ReactDesktopApps2/ReactDesktopApps2.AppWinForms/AppHost.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Funq;
+using ServiceStack;
+using ReactDesktopApps2.Resources;
+using ReactDesktopApps2.ServiceInterface;
+
+namespace ReactDesktopApps2.AppWinForms
+{
+    public class AppHost : AppSelfHostBase
+    {
+        /// <summary>
+        /// Default constructor.
+        /// Base constructor requires a name and assembly to locate web service classes. 
+        /// </summary>
+        public AppHost()
+            : base("ReactDesktopApps2.AppWinForms", typeof(MyServices).Assembly) { }
+
+        /// <summary>
+        /// Application specific configuration
+        /// This method should initialize any IoC resources utilized by your web service classes.
+        /// </summary>
+        /// <param name="container"></param>
+        public override void Configure(Container container)
+        {
+            //Config examples
+            //this.Plugins.Add(new PostmanFeature());
+            //Plugins.Add(new CorsFeature());
+
+            SetConfig(new HostConfig
+            {
+                DebugMode = true,
+                EmbeddedResourceBaseTypes = { typeof(AppHost), typeof(CefResources) },
+            });
+        }
+    }
+}
