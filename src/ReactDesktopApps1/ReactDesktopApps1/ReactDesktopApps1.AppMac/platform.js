35641866c831f4cf03e2316bccf74e230214e5a3
Darren Reid <darren.j.reid@gmail.com>
2015-09-18 21:37:36 +1000

    Mac test

---
+++ src/ReactDesktopApps1/ReactDesktopApps1/ReactDesktopApps1.AppMac/platform.js
@@ -0,0 +1,23 @@
+/* mac */
+document.documentElement.className += ' mac';
+window.nativeHost = {
+    quit: function () {
+        $.get('/nativehost/quit');
+    },
+    showAbout: function () {
+    	$.get('/nativehost/showAbout');
+    },
+    toggleFormBorder: function () {
+        //
+    },
+    dockLeft: function () {
+        //
+    },
+    dockRight: function () {
+        //
+    },
+    ready: function () {
+        //
+    },
+    platform: 'mac'
+};
